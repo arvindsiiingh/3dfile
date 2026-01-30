a0799128d4ec4ab53d1576888f3dca6e8df69d03
Arvind Singh <arvindsinghmems@gmail.com>
2026-01-30 17:52:02 +0530

    Scale 3D model 2.5x for larger display in embed

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ import { OrbitControls } from "https://esm.sh/three@0.169.0/examples/jsm/control
 import { GLTFLoader } from "https://esm.sh/three@0.169.0/examples/jsm/loaders/GLTFLoader.js";
 
 const GLB_PATH = "/bag.glb";
+const MODEL_SCALE = 2.5;
 
 const canvas = document.getElementById("canvas");
 const width = window.innerWidth;
@@ -51,6 +52,7 @@ loader.load(
   GLB_PATH,
   (gltf) => {
     model = gltf.scene;
+    model.scale.setScalar(MODEL_SCALE);
     scene.add(model);
     const box = new THREE.Box3().setFromObject(model);
     const center = box.getCenter(new THREE.Vector3());
